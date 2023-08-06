c9313b24702197b3b7580d5f4cc3b3e5fe516e0f
Tanu ray <132033627+tanukumariray@users.noreply.github.com>
2023-08-06 19:38:29 +0530

    Add files via upload

---
+++ function.java
@@ -0,0 +1,19 @@
+class function
+{
+	public static void main(String args[])
+	{
+	System.out.println("Hello world");
+	fun oc=new fun();	//created Object
+	oc.add();		//created Function
+	}
+}
+class fun
+{
+	public void add()	//function call
+	{
+	int a = 41;
+	int b = 30;
+	int c=a+b;
+	System.out.println("Addition of two no is ="+c);
+	}
+}
